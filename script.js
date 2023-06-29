0a15f9864c6524f0a1fdada1432fed28306a4bf8
valeriamarquezramos2027 <138133204+valeriamarquezramos2027@users.noreply.github.com>
2023-06-29 08:55:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var mybestie="my group"
+mybestie = '10'
+console.log(mybestie);
+var numberofsiblings  = 2
+var hasPets = TRUE; 
+var myname = "valeria marquez";
+if(hasPets === TRUE) { 
+    console.log (" they are joyful -- they have a lot of energy");
+
+}
+if (hasPets === TRUE)[
+    console.log("humman are the goats") 
+    ]
+    var number =0
+    while (nuber <5 )
+        console.log("4 ");
+        number++;
+     var button = document.getElementById("button");
+     var h1 = document.getElementById( "besite")
+     var body = document.body 
+
+     button.addEventListener ("besite")
